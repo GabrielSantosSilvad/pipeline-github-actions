5dea861939c54bf90419aaf8db129f79b2c9c3fc
Gabriel dos Santos Silva <56728463+GabrielSantosSilvad@users.noreply.github.com>
2025-09-15 10:28:32 -0300

    Update vm.tf
    Mudando tipo de instance_type : t2.medium

--- vm.tf
+++ vm.tf
@@ -7,7 +7,7 @@ resource "aws_key_pair" "key_acesso" { #ssh-keygen -f aws-key
 
 resource "aws_instance" "vm" {
   ami                         = "ami-0b8b44ec9a8f90422"
-  instance_type               = "t2.micro"
+  instance_type               = "t2.medium"
   key_name                    = aws_key_pair.key_acesso.key_name
   subnet_id                   = data.terraform_remote_state.vpc.outputs.subnet_id
   vpc_security_group_ids      = [data.terraform_remote_state.vpc.outputs.security_id]
